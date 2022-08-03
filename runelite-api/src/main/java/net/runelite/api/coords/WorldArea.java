c66dae8c808d7b206ba005d3444178ab6b575cad
Leonard Joensen <leonardvdj@gmail.com>
2022-08-03 03:00:15 +0200

    api: Change walker to WorldArea, with WorldPoint overloads

--- runelite-api/src/main/java/net/runelite/api/coords/WorldArea.java
+++ runelite-api/src/main/java/net/runelite/api/coords/WorldArea.java
@@ -29,6 +29,7 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Predicate;
 import lombok.Getter;
+import lombok.Value;
 import net.runelite.api.Client;
 import net.runelite.api.CollisionData;
 import net.runelite.api.CollisionDataFlag;
@@ -40,6 +41,7 @@ import net.runelite.api.Constants;
 /**
  * Represents an area on the world.
  */
+@Value
 public class WorldArea
 {
 	/**
